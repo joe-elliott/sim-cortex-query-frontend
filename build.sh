6325c6a2765505c59d671fd6be021e58d4db41d6
Joe Elliott <number101010@gmail.com>
2020-04-10 11:49:31 -0400

    Added k3d setup
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 pushd frontend 
-docker build . -t sim-frontend:latest
+docker build . -t registry.local:5000/sim-frontend:latest
+docker push registry.local:5000/sim-frontend:latest
 popd
 
 pushd worker
-docker build . -t sim-worker:latest
+docker build . -t registry.local:5000/sim-worker:latest
+docker push registry.local:5000/sim-worker:latest
 popd
 
 pushd loadgen
-docker build . -t sim-loadgen:latest
+docker build . -t registry.local:5000/sim-loadgen:latest
+docker push registry.local:5000/sim-loadgen:latest
 popd
